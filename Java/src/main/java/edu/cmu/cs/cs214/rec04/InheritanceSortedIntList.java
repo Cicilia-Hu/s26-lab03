c6c90781d87cc59fb028c9a196f8c696cbb0b70e
胡可嘉 <hukejia@kokayoshimis-MacBook-Air-2.local>
2026-01-29 15:18:31 -0500

    complete task 1

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -11,7 +11,40 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
     // Write your implementation below with API documentation
+
+    /**
+     * count of the number of attempted element insertions
+     */ 
+    private int totalAdded = 0;
+    
+    /**
+     * return the number of attempted insertions
+     */ 
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    /**
+     * Adds one element to the list
+     * increment the totalAdded
+     * return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        totalAdded += 1;
+        return super.add(num);
+    }
+
+    /**
+     * Adds all elements of list into this list
+     * return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        return super.addAll(list);
+    }
+
 
 }
