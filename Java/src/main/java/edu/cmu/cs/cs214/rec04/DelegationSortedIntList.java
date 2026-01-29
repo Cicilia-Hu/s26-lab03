fcdff61ecfba8bcc3b1084e032cc1232e444c81c
胡可嘉 <hukejia@kokayoshimis-MacBook-Air-2.local>
2026-01-29 15:54:16 -0500

    complete task 2

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,95 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
 
+    private final SortedIntList inner;
+
+    /**
+     * count of the number of attempted element insertions
+     */ 
+    private int totalAdded;
+
+    public DelegationSortedIntList() {
+        this.inner = new SortedIntList();
+        this.totalAdded = 0;
+    }
+
+    /**
+     * return the number of attempted insertions
+     */ 
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    /**
+     * Adds one element to the list
+     * increment the totalAdded
+     * return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        totalAdded += 1;
+        return inner.add(num);
+    }
+
+    /**
+     * Adds all elements of list into this list
+     * return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        totalAdded += list.size();
+        return inner.addAll(list);
+    }
+
+
+
+    /**
+     * Returns the integer at the specified position in this list.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     */
+    @Override
+    public int get(int index) {
+        return inner.get(index);
+    }
+
+    /**
+     * Removes the first occurrence of the specified element from the list,
+     * if it is present (optional operation).
+     *
+     * @param num an integer to be removed from the list, if present
+     * @return true if an element was removed as a result of this call
+     */
+    @Override
+    public boolean remove(int num) {
+        return inner.remove(num);
+    }
+
+    /**
+     * Removes from the list all of its elements that are contained in the
+     * specified IntegerList.
+     *
+     * @param list IntegerList containing elements to be removed from
+     * the list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return inner.removeAll(list);
+    }
+
+    /**
+     * Returns the number of elements in this list. If this list contains
+     * more than Integer.MAX_VALUE elements, returns Integer.MAX_VALUE.
+     *
+     * @return number of elements in the list
+     */
+    @Override
+    public int size() {
+        return inner.size();
+    }
 }
